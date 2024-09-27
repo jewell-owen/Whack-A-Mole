f318864ce6e4df456dc07747e27786c86923466a
jewell-owen <jewell.owen@yahoo.com>
2024-09-27 12:41:43 -0500

    changes

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -25,5 +27,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         newGameBtn = findViewById(R.id.main_new_game_btn);
+
+        newGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
